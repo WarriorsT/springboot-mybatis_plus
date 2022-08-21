393af8e0b8380ee8f0770728ae60142e75e88083
WarriorsT <109058987+WarriorsT@users.noreply.github.com>
2022-08-21 17:00:16 +0800

    Add files via upload

---
+++ week-04/src/main/java/com/backend/pojo/User.java
@@ -0,0 +1,27 @@
+package com.backend.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String username;
+    private String password;
+    private String studentNo;
+    private String institution;
+    private String grade;
+    private String classInfo;
+    private String name;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+}
